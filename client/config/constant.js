686576a8900c282fe08ce5c6c6ce3faee66c9a08
ila <ila@ila-empire.(none)>
2011-10-19 22:39:37 +0800

    add expressjs

--- client/config/constant.js
+++ client/config/constant.js
@@ -1,1 +1,1 @@
-global.GI_SERVER = "ws://192.168.11.123:8222";
+global.GI_SERVER = "ws://cobejeweled:8222";
--- server/server.js
+++ server/server.js
@@ -1,21 +1,36 @@
 require('./require');
+/* ExpressJS Start */
+var express = require('express');
+var app = express.createServer();
+app.use(express.bodyParser());
+app.use(app.router);
+app.use(express.static('/home/ila/project/cobejeweled/client/'));
+var downloads = {};
+app.get('/*', function(req, res, next){
+    var file = req.params[0];
+    console.log(file);
+    downloads[file] = downloads[file] || 0;
+    ++downloads[file];
+    next();
+});
+app.listen(SERVER_LISTEN_PORT);
+/* ExpressJS End */
 
 var WebSocketServer = require('websocket').server;
-var http = require('http');
-var server = http.createServer();
-server.listen(SERVER_LISTEN_PORT);
 
 wsServer = new WebSocketServer({
-    httpServer: server,
+    httpServer: app,
     autoAcceptConnections: false
 });
 
 wsServer.on('request', function(request) {
+    /*
     if (!originIsAllowed(request.origin)) {
         request.reject();
         console.log((new Date()) + " Connection from origin " + request.origin + " rejected.");
         return;
     }
+    */
 
     var connection = request.accept(null, request.origin);
     var cID = Users.newConnection(connection);
