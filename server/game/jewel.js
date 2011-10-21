0c94926b5849fe42d7d7b0e09fb901b27017e113
Dya <dya.ilaempire@gmail.com>
2011-10-21 13:32:57 +0800

    effect added explode|cross|sameClear

--- server/game/jewel.js
+++ server/game/jewel.js
@@ -3,6 +3,28 @@ var JEWELCOUNTY = 13;
 
 var Jewel = function() {
     this.jewels = {};
+    /*
+     *  Gem Type : 
+     *  0 : 'orange'
+     *  1 : 'red'
+     *  2 : 'white'
+     *  3 : 'yellow'
+     *  4 : 'blue'
+     *  5 : 'purple'
+     *  6 : 'green'
+     *  7 : 'grey'
+     *
+     *  Effect Type :
+     *  1 : 'explode'
+     *  2 : 'cross'
+     *  3 : 'sameClear'
+     *
+     */
+    this.effects = {
+        1 : 'doEffectExplode'
+        , 2 : 'doEffectCross'
+        , 3 : 'doEffectSameClear'
+    };
 }
 Jewel.prototype.initFillingUp = function() {
     var jewelIndex, jewelType;
@@ -10,7 +32,7 @@ Jewel.prototype.initFillingUp = function() {
         for (var j = 0; j < JEWELCOUNTY; ++j) {
             jewelIndex = i + ',' + j;
             jewelType = this.createSingleJewel();
-            this.jewels[jewelIndex] = jewelType;
+            this.jewels[jewelIndex] = { type : jewelType, effect : null };
         }
     }
     do {
@@ -31,23 +53,28 @@ Jewel.prototype.clientMvSingleJewel = function(src, tar) {
 
     var srcIndex = src.x + ',' + src.y;
     var tarIndex = tar.x + ',' + tar.y;
-    var srcType = this.jewels[srcIndex];
-    var tarType = this.jewels[tarIndex];
+    var srcType = this.jewels[srcIndex].type;
+    var tarType = this.jewels[tarIndex].type;
 
-    this.jewels[srcIndex] = tarType;
-    this.jewels[tarIndex] = srcType;
+    this.jewels[srcIndex].type = tarType;
+    this.jewels[tarIndex].type = srcType;
 
     if (this.getTriples()) return true;
 
-    this.jewels[srcIndex] = srcType;
-    this.jewels[tarIndex] = tarType;
+    this.jewels[srcIndex].type = srcType;
+    this.jewels[tarIndex].type = tarType;
 
     return false;
 }
 Jewel.prototype.eliminateTriples = function(toEliminateJewels) {
+    toEliminateJewels = this.addEffectCross(toEliminateJewels);
     var length = toEliminateJewels.length;
     for (var i = 0; i < length; ++i) {
-        delete this.jewels[toEliminateJewels[i]];
+        if (!this.jewels[toEliminateJewels[i]]) continue;
+        delete this.jewels[toEliminateJewels[i]].type;
+        if (!this.jewels[toEliminateJewels[i]].effect) continue;
+        this.effects[this.jewels[toEliminateJewels[i]].effect](toEliminateJewels[i]);
+        delete this.jewels[toEliminateJewels[i]].effect;
     }
 }
 Jewel.prototype.coordinateTriplesX = function(coordinateX, coordinateY) {
@@ -57,10 +84,12 @@ Jewel.prototype.coordinateTriplesX = function(coordinateX, coordinateY) {
         eliminateJewels.push(curIndex);
         ++coordinateX;
         var NextIndex = coordinateX + ',' + coordinateY;
-        var curType = this.jewels[curIndex];
-        var NextType = this.jewels[NextIndex];
+        var curType = this.jewels[curIndex].type;
+        var NextType = this.jewels[NextIndex].type;
     } while(curType === NextType);
     if (eliminateJewels.length  < 3) return false;
+    if (eliminateJewels.length === 4) eliminateJewels = this.addEffectExplode(eliminateJewels);
+    if (eliminateJewels.length >= 5) eliminateJewels = this.addEffectSameClear(eliminateJewels);
     return eliminateJewels;
 }
 Jewel.prototype.coordinateTriplesY = function(coordinateX, coordinateY) {
@@ -70,10 +99,12 @@ Jewel.prototype.coordinateTriplesY = function(coordinateX, coordinateY) {
         eliminateJewels.push(curIndex);
         ++coordinateY;
         var NextIndex = coordinateX + ',' + coordinateY;
-        var curType = this.jewels[curIndex];
-        var NextType = this.jewels[NextIndex];
+        var curType = this.jewels[curIndex].type;
+        var NextType = this.jewels[NextIndex].type;
     } while(curType === NextType);
     if (eliminateJewels.length  < 3) return false;
+    if (eliminateJewels.length === 4) eliminateJewels = this.addEffectExplode(eliminateJewels);
+    if (eliminateJewels.length >= 5) eliminateJewels = this.addEffectSameClear(eliminateJewels);
     return eliminateJewels;
 }
 Jewel.prototype.getTriples = function() {
@@ -91,7 +122,7 @@ Jewel.prototype.getTriples = function() {
 Jewel.prototype.jewelsReorganize = function() {
     var falls = [];
     for (var index in this.jewels) {
-        if (!this.jewels[index] && this.jewels[index] != 0) continue;
+        if (!this.jewels[index].type && this.jewels[index].type != 0) continue;
         falls.push(this.doJewelFall(index));
     }
     return falls;
@@ -102,16 +133,18 @@ Jewel.prototype.doJewelFall = function(index) {
     var indexXY = fc.getXY(index);
     var fallToCoordinateY = indexXY.y - emptyCount;
     var fallToIndex = indexXY.x + ',' + fallToCoordinateY;
-    this.jewels[fallToIndex] = this.jewels[index];
-    delete this.jewels[index];
+    this.jewels[fallToIndex].type = this.jewels[index].type;
+    delete this.jewels[index].type;
     var singleFall = {
         from : { 
             index : index
-            , type : this.jewels[index]
+            , type : this.jewels[index].type
+            , effect : this.jewels[index].effect
         }
         , to : {
             index : fallToIndex
-            , type : this.jewels[fallToIndex]
+            , type : this.jewels[fallToIndex].type
+            , effect : this.jewels[fallToIndex].effect
         }
     };
     return singleFall;
@@ -123,7 +156,7 @@ Jewel.prototype.getBelowEmptyCount = function(index) {
     while (coordinateY < JEWELCOUNTY) {
         --coordinateY;
         var indexBelow = indexXY.x + ',' + coordinateY;
-        if (this.jewels[indexBelow] || this.jewels[indexBelow] === 0) continue;
+        if (this.jewels[indexBelow].type || this.jewels[indexBelow].type === 0) continue;
         ++emptyCount;
     }
     return emptyCount;
@@ -132,16 +165,18 @@ Jewel.prototype.fillEmptyJewels = function() {
     var filled = [];
     for (var index in this.jewels) {
         if (!this.isJewelEmpty()) continue;
-        this.jewels[index] = this.createSingleJewel();
+        this.jewels[index].type = this.createSingleJewel();
         var singleFilled = {
-            index : this.jewels[index]
+            index : index
+            type : this.jewels[index].type
+            effect : this.jewels[index].effect
         };
         filled.push(singleFilled);
     }
     return filled;
 }
 Jewel.prototype.isJewelEmpty = function() {
-    if (this.jewels[index] || this.jewels[index] === 0) return false;
+    if (this.jewels[index].type || this.jewels[index].type === 0) return false;
     return true;
 }
 Jewel.prototype.checkNeighbor = function(src, tar) {
@@ -156,6 +191,58 @@ Jewel.prototype.verifyCoordinate = function(coord) {
 }
 Jewel.prototype.createSingleJewel = function() {
     return Math.round(Math.random() * 7);
+}
+Jewel.prototype.addEffectExplode = function(toEliminateJewels) {
+    if (toEliminateJewels != 4) return;
+    var constGenerateIndex = 1;
+    var index = toEliminateJewels[constGenerateIndex];
+    if (this.jewels[index].effect) return;  // TODO index effect exist fix
+    this.jewels[index].effect = 1;
+    delete toEliminateJewels[constGenerateIndex];
+    return toEliminateJewels;
+}
+Jewel.prototype.addEffectCross = function(toEliminateJewels) {
+    var crossPoint = fc.hasRepeat(toEliminateJewels);
+    for (var x in crossPoint) {
+        var index = crossPoint[x];
+        if (this.jewels[index].effect) continue;    // TODO
+        thi.jewels[index].effect = 2;
+        delete toEliminateJewels[x];
+    }
+    return toEliminateJewels;
+}
+Jewel.prototype.addEffectSameClear = function(toEliminateJewels) {
+    if (toEliminateJewels < 5) return;
+    var constGenerateIndex = 2;
+    var index = toEliminateJewels[constGenerateIndex];
+    if (this.jewels[index].effect) return;  // TODO
+    this.jewels[index].effect = 3;
+    delete toEliminateJewels[constGenerateIndex];
+    return toEliminateJewels;
+}
+Jewel.prototype.doEffectExplode = function(index) {
+    var box = fc.getNineBlockBox(index);
+    for (var x in box) {
+        if (!this.jewels[box[x]]) delete box[x];
+    }
+    return box;
+}
+Jewel.prototype.doEffectCross = function(index) {
+    var crossPoint = fc.getXY(index);
+    var cross = [];
+    for (var gem in this.jewels) {
+        var gemXY = fc.getXY(this.jewels[gem]);
+        if (crossPoint.x === gemXY.x || crossPoint.y === gemXY.y) cross.push(gem);
+    }
+    return cross;
+}
+Jewel.prototype.doEffectSameClear = function(index) {
+    var type = this.jewels[index].type;
+    var clear = [];
+    for (var gem in this.jewels) {
+        if (this.jewels[gem].type === type) clear.push(gem);
+    }
+    return clear;
 }
 
 global.CoBejeweled = new Jewel;
--- server/lib/facility.js
+++ server/lib/facility.js
@@ -13,39 +13,39 @@ global.fc = {
         return guid; 
     }
     //get random
-    ,random : function(n) {
+    , random : function(n) {
         return Math.floor(Math.random() * n);
     }
     //get timestamp
-    ,getTimestamp : function() {
+    , getTimestamp : function() {
         return Date.now();
     }
     //get object length
-    ,objectLength : function(object) {
+    , objectLength : function(object) {
         var c = 0;
         for (var i in object) {
             if (object.hasOwnProperty(i)) ++c;
         }
         return c;
     }
-    ,getXY : function(index) {
+    , getXY : function(index) {
         index = index.split(',');
         return {x : parseInt(index[0]), y : parseInt(index[1])};
     }
-    ,getIndex : function(x, y) {
+    , getIndex : function(x, y) {
         return x + ',' + y;
     }
-    ,fix : function(float) {
+    , fix : function(float) {
         return typeof(float) === 'number' ? parseInt(float.toFixed()) : float;
     }
-    ,readFile : function(path) {
+    , readFile : function(path) {
         var content = fs.readFileSync(path, 'utf8');
         var array = content.split("\n");
         delete array[0];
         content = array.join("\n");
         return JSON.parse(content);
     }
-    ,fill0 : function(length, origin) {
+    , fill0 : function(length, origin) {
         var originLength = ('' + origin).length;
         var fillTime = length - originLength;
         var string = origin;
@@ -54,7 +54,62 @@ global.fc = {
         }
         return string;
     }
-    ,encode : function(msg) {
+    , encode : function(msg) {
         return JSON.stringify(msg);
     }
+    , hasRepeat : function(array) {
+        var hash = {};
+        var repeated = {};
+        for (var i = 0; i < array.length; ++i) {
+            if (hash[array[i]]) repeated[i] = array[i];
+            hash[array[i]] = true;
+        }
+        return repeated;
+    }
+    , getNineBlockBox : function(index) {
+        var indexXY = this.getXY(index);
+        var box = [];
+        var defineBox = { 
+            0 : {
+                x : -1
+                , y : -1
+            }
+            , 1 : {
+                x : 0
+                , y : -1
+            }
+            , 2 : {
+                x : 1
+                , y : -1
+            }
+            , 3 : {
+                x : 1
+                , y : 0
+            }
+            , 4 : {
+                x : 1
+                , y : 1
+            }
+            , 5 : {
+                x : 0
+                , y : 1
+            }
+            , 6 : {
+                x : -1
+                , y : 1
+            }
+            , 7 : {
+                x : -1
+                , y : 0
+            }
+        };
+        for (var pos in defineBox) {
+            var x = indexXY.x + defineBox[pos].x;
+            var y = indexXY.y + defineBox[pos].y;
+            var blockIndex = x + ',' + y;
+            box.push(blockIndex);
+        }
+        return box;
+    }
+
 }
