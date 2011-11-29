caebf0a9b053ac6506c8ebbbcd41d1eba8dd3327
dya Chang <dya.ilaempire@gmail.com>
2011-11-29 09:34:35 +0800

    some fix

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express.createServer();
 app.use(express.bodyParser());
 app.use(app.router);
-app.use(express.static('/home/dya/workspace/localhost/CoBejeweled/client/'));
+app.use(express.static('/home/dya/workspace/Cobejeweled/client/'));
 //app.use(express.static('/home/ila/project/cobejeweled/client/'));
 var downloads = {};
 app.get('/*', function(req, res, next){
@@ -25,14 +25,6 @@ wsServer = new WebSocketServer({
 });
 
 wsServer.on('request', function(request) {
-    /*
-    if (!originIsAllowed(request.origin)) {
-        request.reject();
-        console.log((new Date()) + " Connection from origin " + request.origin + " rejected.");
-        return;
-    }
-    */
-
     var connection = request.accept(null, request.origin);
     ioExcute.process(connection, fc.encode({ protocol : 'newLogin', data : {} }));
     ioExcute.process(connection, fc.encode({ protocol : 'roomList', data : {} }));
@@ -45,8 +37,3 @@ wsServer.on('request', function(request) {
         ioExcute.process(conn, fc.encode({ protocol : 'disconnect', data : {} }));
     });
 });
-
-function originIsAllowed(origin) {
-    if (origin === 'http://www.cobejeweled.com') return true;
-    return false;
-}
